63bde67642f810acdbb6c39b4fb163678b4f9041
kphan <kphan@hotmail.fr>
2025-07-04 08:46:22 +0200

    add json

--- script/script.js
+++ script/script.js
@@ -1,16 +1,3 @@
-// Function to add the "navbarDark" class to the navbar on scroll
-function handleNavbarScroll() {
-    const header = document.querySelector(".navbar");
-    window.onscroll = function () {
-        const top = window.scrollY;
-        if (top >= 100) {
-            header.classList.add("navbarDark");
-        } else {
-            header.classList.remove("navbarDark");
-        }
-    };
-}
-
 // Function to handle navbar collapse on small devices after a click
 function handleNavbarCollapse() {
     const navLinks = document.querySelectorAll(".nav-item");
@@ -60,13 +47,49 @@ function createSkillsFromJSON() {
         });
 }
 // Function to dynamically create HTML elements from the JSON file
-function createPortfolioFromJSON() {
-    const container = document.querySelector("#portfolio .container");
+function createToolsFromJSON() {
+    const container = document.querySelector("#tools .container");
     let row = document.createElement("div");
     row.classList.add("row");
 
     // Load the JSON file
-    fetch("data/portfolio.json")
+    fetch("/data/tools.json")
+        .then((response) => response.json())
+        .then((data) => {
+            // Iterate through the JSON data and create HTML elements
+            data.forEach((item, index) => {
+                const card = document.createElement("div");
+                card.classList.add("col-lg-4", "mt-4");
+                card.innerHTML = `
+                    <div class="card toolsText">
+                        <div class="card-body">
+                            <img src="./images/${item.image}" alt="logo-${item.title}" />
+                            <h3 class="card-title mt-3">${item.title}</h3>
+                            <p class="card-text mt-3">${item.text}</p>
+                        </div>
+                    </div>
+                `;
+
+                // Append the card to the current row
+                row.appendChild(card);
+
+                // If the index is a multiple of 3 or it's the last element, create a new row
+                if ((index + 1) % 3 === 0 || index === data.length - 1) {
+                    container.appendChild(row);
+                    row = document.createElement("div");
+                    row.classList.add("row");
+                }
+            });
+        });
+}
+// Function to dynamically create HTML elements from the JSON file
+function createPortfolioDevFromJSON() {
+    const container = document.querySelector("#portfolioDev .container");
+    let row = document.createElement("div");
+    row.classList.add("row");
+
+    // Load the JSON file
+    fetch("data/portfolioDev.json")
         .then((response) => response.json())
         .then((data) => {
             // Iterate through the JSON data and create HTML elements
@@ -80,7 +103,47 @@ function createPortfolioFromJSON() {
                         <h3 class="card-title">${item.title}</h3>
                         <p class="card-text">${item.text}</p>
                         <div class="text-center">
-                            <a href="${item.link}" class="btn btn-success">Lien</a>
+                            <a href="${item.link}" class="btn btn-success" target="_blank">Lien</a>
+                        </div>
+                    </div>
+                </div>
+                `;
+
+                // Append the card to the current row
+                row.appendChild(card);
+
+                // If the index is a multiple of 3 or it's the last element, create a new row
+                if ((index + 1) % 3 === 0 || index === data.length - 1) {
+                    container.appendChild(row);
+                    row = document.createElement("div");
+                    row.classList.add("row");
+                }
+            });
+        });
+}
+
+// Function to dynamically create HTML elements from the JSON file
+function createPortfolioTestFromJSON() {
+    const container = document.querySelector("#portfolioTest .container");
+    let row = document.createElement("div");
+    row.classList.add("row");
+
+    // Load the JSON file
+    fetch("data/portfolioTest.json")
+        .then((response) => response.json())
+        .then((data) => {
+            // Iterate through the JSON data and create HTML elements
+            data.forEach((item, index) => {
+                const card = document.createElement("div");
+                card.classList.add("col-lg-4", "mt-4");
+                card.innerHTML = `
+                    <div class="card portfolioContent">
+                    <img class="card-img-top" src="images/${item.image}" alt="image-${item.title}" style="width:100%">
+                    <div class="card-body">
+                        <h3 class="card-title">${item.title}</h3>
+                        <p class="card-text">${item.text}</p>
+                        <div class="text-center">
+                            <a href="${item.link}" class="btn btn-success" target="_blank">Lien</a>
                         </div>
                     </div>
                 </div>
@@ -100,7 +163,9 @@ function createPortfolioFromJSON() {
 }
 
 // Call the functions to execute the code
-handleNavbarScroll();
 handleNavbarCollapse();
 createSkillsFromJSON();
-createPortfolioFromJSON();
+createToolsFromJSON();
+createPortfolioDevFromJSON();
+createPortfolioTestFromJSON();
+
